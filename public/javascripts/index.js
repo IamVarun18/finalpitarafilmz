2608bd3be79a153fbbd3405948a2c16ec8d594cc
IamVarun18 <130641848+IamVarun18@users.noreply.github.com>
2024-02-08 00:39:36 +0530

    lets talk

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -307,3 +307,42 @@ document.addEventListener('DOMContentLoaded', function () {
     mainContent.style.display = 'block';
   });
 });
+
+
+const form = document.querySelector('form'); // Corrected the selector
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const requirements = document.getElementById("requirements");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value}<br>Email: ${email.value}<br>Phone Number: ${number.value}<br>`;
+  Email.send({
+    SecureToken: "b23bb175-558d-49e3-9bcc-9a4d08c58f7f",
+    // Host: "smtp.elasticemail.com",
+    // Username: "ashishshri3096@gmail.com",
+    // Password: "D57057254D6C0AF64FCF15BABA50243EB0EE",
+    // this blow is varun username and pass 
+    // Username: "varun.018072001@gmail.com",
+    // Password: "D0490486A822D50A770C2E37C6C7D8A00241",
+    To: 'ashishshri3096@gmail.com',
+    From: "varun.018072001@gmail.com",
+    Subject: requirements.value,
+    Body: bodyMessage
+  }).then(
+    message => {
+      if (message == "OK") {
+        Swal.fire({
+          title: "Sucess!",
+          text: "Message Sent Successfully!",
+          icon: "success"
+        });
+      }
+    }
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
